0cca853efb1982a539cdd762915cb03b976ab164
lcornejo666 <leonardocornejo23@gmail.com>
2023-07-31 18:08:14 +0200

    subir

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+mario.onclick = function(jugar) {
+
+    consola.src="https://supernintendoemulator.com/game/?game=Super_Mario_World"
+}
+
+kong.onclick = function(jugar) {
+
+    consola.src="https://supernintendoemulator.com/game/?game=Donkey_Kong_Country"
+}
+
+bomberman.onclick = function(jugar) {
+
+    consola.src="https://supernintendoemulator.com/game/?game=Super_Bomberman_5_Gold_Cartridge"
+}
+
+contra.onclick = function(jugar) {
+
+    consola.src="https://supernintendoemulator.com/game/?game=Contra_III_-_The_Alien_Wars"
+}
+
+pelea.onclick = function(jugar) {
+
+    consola.src="https://supernintendoemulator.com/game/?game=Street_Fighter_II_Turbo_-_Hyper_Fighting"
+}
+
+autos.onclick = function(jugar) {
+
+    consola.src="https://www.minijuegos.com/embed/top-gear"
+}
+
+
+
+/*
+let ven = document.getElementById("ven");
+let dep = document.getElementById("dep");
+let cap = document.getElementById("cap");
+let gat = document.getElementById("gat");
+let iro = document.getElementById("iro");
+let bat = document.getElementById("bat");
+let imgcasco = document.getElementById("imgcasco");
+
+ven.onclick = function() {
+
+    imgcasco.src="imagen/venon.png"
+}
+
+dep.onclick = function() {
+
+    imgcasco.src="imagen/depredador.png"
+}
+
+cap.onclick = function() {
+
+    imgcasco.src="imagen/capitanamerica.png"
+}
+
+gat.onclick = function() {
+
+    imgcasco.src="imagen/gatubela.png"
+}
+
+iro.onclick = function() {
+
+    imgcasco.src="imagen/ironman.png"
+}
+
+bat.onclick = function() {
+
+    imgcasco.src="imagen/batman.png"
+}
+*/
